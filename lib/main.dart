d7d4f4aac3b419a6215ac73abe916ad24bd8d880
Gagandeep <gagandeepkaur1523@gmail.com>
2023-01-19 22:52:26 +0530

    Added sizer to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notes/notes.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,30 +25,34 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 84, 161, 76),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-          title: Text(
-            'NOTES',
-            style: GoogleFonts.rubik(
-              fontWeight: FontWeight.bold,
-              color: const Color.fromARGB(255, 0, 0, 0),
+    return Sizer(
+      builder: ((context, orientation, deviceType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            backgroundColor: const Color.fromARGB(255, 84, 161, 76),
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              title: Text(
+                'NOTES',
+                style: GoogleFonts.rubik(
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                ),
+              ),
+            ),
+            body: const NotesCard(),
+
+            // FLOATING ACTION BUTTON TO ADD NOTES
+            floatingActionButton: FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              backgroundColor: const Color.fromARGB(255, 255, 246, 169),
+              child: const Icon(Icons.add),
             ),
           ),
-        ),
-        body: const NotesCard(),
-
-        // FLOATING ACTION BUTTON TO ADD NOTES
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          backgroundColor: const Color.fromARGB(255, 255, 246, 169),
-          child: const Icon(Icons.add),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
